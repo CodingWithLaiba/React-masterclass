0574fdd2b1ccc9dafa084adde124b0e3414ee3b2
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-22 20:20:21 +0500

    feat: refactor Hello component usage to pass person objects

--- 2-Props/src/App.jsx
+++ 2-Props/src/App.jsx
@@ -1,10 +1,22 @@
 import Hello from "./components/Hello";
 
 function App() {
+  // const setNumber = [1, 2, 3];
+  const person1 = {
+    message: "Hiii",
+    name: "Rob",
+    setNumber: [1, 2, 3],
+  };
+  const person2 = {
+    message: "Hello",
+    name: "nike",
+    setNumber: [1, 3, 3],
+  };
+
   return (
     <>
-      <Hello message= "Hiii" name="Rob" />
-      <Hello message="Hello" name="Nick" />
+      <Hello person={person1} />
+      <Hello person={person2} />
     </>
   );
 }
