ad9b749070aa1ad55a5bd7a8c93e04bcc941b190
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-22 21:30:08 +0500

    feat: update Furits component to render array of objects with price and emoji

--- 3_RendringArray/src/components/furits.jsx
+++ 3_RendringArray/src/components/furits.jsx
@@ -1,12 +1,27 @@
 export default function Furits() {
-  const furits = ["Apple", "Mango", "Banana", "Orange", "Peach"];
-
+  //   const furits = ["Apple", "Mango", "Banana", "Orange", "Peach"];
+  const furits = [
+    { name: "Apple", price: "10", emoji: "🍎" },
+    { name: "Mango", price: "3", emoji: "🥭" },
+    { name: "Banana", price: "11", emoji: "🍌" },
+    { name: "Orange", price: "9", emoji: "🍊" },
+    { name: "Peach", price: "4", emoji: "🍑" },
+  ];
   return (
     <>
       <div>
-       
-        {furits.map((fruit) => (
+        {/* for rendring simple array */}
+
+        {/* {furits.map((fruit) => (
           <h4 key={fruit}>{fruit}</h4>
+        ))} */}
+
+        {/* for rendring array of objs */}
+
+        {furits.map((furit) => (
+          <h5 key={furit.name}>
+            {furit.emoji} {furit.name} ${furit.price}
+          </h5>
         ))}
       </div>
     </>
