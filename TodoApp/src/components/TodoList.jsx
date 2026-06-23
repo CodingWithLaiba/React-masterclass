781ba2e122db1f44588280431808bad8448844ea
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-23 22:14:20 +0500

    feat: implement delete functionality in TodoList component

--- TodoApp/src/components/TodoList.jsx
+++ TodoApp/src/components/TodoList.jsx
@@ -1,3 +1,20 @@
-export default function TodoList({ item }) {
-  return <div>{item}</div>;
+export default function TodoList({ item, todoList, setTodoList }) {
+  function handleDelete(item) {
+    console.log(item, "Deleted");
+    setTodoList(todoList.filter((todo) => todo !== item)); //filter make the new of after deleting the specific item
+  }
+  return (
+    <div className="w-125 text-[#353935] text-xl mx-auto px-6">
+      <div className="flex justify-between items-center font-bold text-xl py-5">
+        <span>{item}</span>
+        <button
+          onClick={() => handleDelete(item)}
+          className="bg-amber-300 text-white border-none py-2.5 px-4.5 rounded-sm cursor-pointer"
+        >
+          X
+        </button>
+      </div>
+      <hr className="border border-[#f4f4f4]" />
+    </div>
+  );
 }
