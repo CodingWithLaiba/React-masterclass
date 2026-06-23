511f82a0ce28a05fd5ee5486131a073e60e80654
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-23 13:26:45 +0500

    feat: add soldout prop to Fruit component for better inventory display

--- 3_RendringArray/src/components/Fruit.jsx
+++ 3_RendringArray/src/components/Fruit.jsx
@@ -1,7 +1,15 @@
-export default function Fruit({ name, emoji, price }) {
+export default function Fruit({ name, emoji, price, soldout }) {
   return (
     <div>
-      {emoji} {name} {price}
+      {/* {emoji} {name} {price} */}
+      {/* {price > 5 ? (
+        <>
+          {emoji} {name} {price}
+        </>
+      ) : (
+        ""
+      )} */}
+      {emoji} {name} {price} {soldout ? "soldout" : ""}
     </div>
   );
 }
