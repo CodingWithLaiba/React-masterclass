a1a5b981fa2f4869a14d5ae00edb48cfa7b9e809
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-23 22:14:10 +0500

    feat: enhance Todo component styling and structure

--- TodoApp/src/components/Todo.jsx
+++ TodoApp/src/components/Todo.jsx
@@ -11,18 +11,35 @@ export default function Todo() {
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        className="bg-white mt-12 rounded-md p-5 m-auto shadow-xl w-125 flex"
+        onSubmit={handleSubmit}
+      >
         <input
+          className="block w-full border-none text-black text-[16px] focus:outline-0"
+          placeholder="Enter TodoList ..."
           type="text"
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
         />
-        <button type="submit">Add</button>
+        <button
+          className="border-none bg-yellow-300 inline-block rounded-sm text-white py-2.5 px-4 cursor-pointer"
+          type="submit"
+        >
+          Add
+        </button>
       </form>
-      {todoList.map((item) => (
-        // <h3 key={item}>{item}</h3>
-        <TodoList item={item} key={item} />
-      ))}
+      <div className="bg-white rounded-md w-125 mt-12 mx-auto shadow-2xl">
+        {todoList.map((item) => (
+          // <h3 key={item}>{item}</h3>
+          <TodoList
+            item={item}
+            key={item}
+            todoList={todoList}
+            setTodoList={setTodoList}
+          />
+        ))}
+      </div>
     </div>
   );
 }
