96ff520c70c967c58d248c08d56703a89578fe42
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-24 16:06:15 +0500

    feat: enhance TodoList component to handle completion state and improve item display

--- TodoApp/src/components/TodoList.jsx
+++ TodoApp/src/components/TodoList.jsx
@@ -3,10 +3,21 @@ export default function TodoList({ item, todoList, setTodoList }) {
     console.log(item, "Deleted");
     setTodoList(todoList.filter((todo) => todo !== item)); //filter make the new of after deleting the specific item
   }
+  function handleComplete(name) {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.name === name ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  }
+  const cName = item.completed ? "line-through text-gray-400" : "";
+
   return (
     <div className="w-125 text-[#353935] text-xl mx-auto px-6">
       <div className="flex justify-between items-center font-bold text-xl py-5">
-        <span>{item}</span>
+        <span className={cName} onClick={() => handleComplete(item.name)}>
+          {item.name}
+        </span>
         <button
           onClick={() => handleDelete(item)}
           className="bg-amber-300 text-white border-none py-2.5 px-4.5 rounded-sm cursor-pointer"
