3c9054f906a859945baffa83fa0100fc959e1cea
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-25 17:36:37 +0500

    feat: enhance useReducer implementation to manage increment value dynamically

--- 7-UseReducer/src/App.jsx
+++ 7-UseReducer/src/App.jsx
@@ -14,12 +14,15 @@ export default function App() {
   // }
 
   // useReducer using the complex object
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 1 });
   function reducer(state, action) {
     if (action.type == "Increment") {
-      return { ...state, count: state.count + action.payload };
+      return { ...state, count: state.count + state.incrementBy };
     } else if (action.type == "decrement") {
       return { ...state, count: state.count - action.payload };
+    }
+    if (action.type == "setIncrement") {
+      return { ...state, incrementBy: action.payload };
     }
   }
   return (
@@ -32,6 +35,13 @@ export default function App() {
       <div>
         {/* <p>{state}</p> */}
         <p>{state.count}</p>
+        <input
+          type="text"
+          value={state.incrementBy}
+          onChange={(e) =>
+            dispatch({ type: "setIncrement", payload: Number(e.target.value) })
+          }
+        />
         <button onClick={() => dispatch({ type: "Increment", payload: 1 })}>
           Increment
         </button>
