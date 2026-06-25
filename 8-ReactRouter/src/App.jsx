3e914a492df58020b4a23d0522b8e698d71a2ce0
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-25 20:50:22 +0500

    feat: implement routing with BrowserRouter and add main application structure

--- 8-ReactRouter/src/App.jsx
+++ 8-ReactRouter/src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/navbar";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import Setting from "./pages/Setting";
+import Product from "./pages/Product";
+
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="profile" element={<Profile />} /> {/* Nested Route */}
+            <Route path="settings" element={<Setting />} /> {/* Nested Route */}
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
