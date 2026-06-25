a0ae691a027c079258b25573b73f560010ce9552
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-25 17:18:10 +0500

    feat: refactor App component to implement useReducer with a complex object for state management

--- 7-UseReducer/src/App.jsx
+++ 7-UseReducer/src/App.jsx
@@ -1,25 +1,37 @@
 import { useReducer, useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [state, dispatch] = useReducer(reducer, 0);
+  // const [count, setCount] = useState(0); // count increment with useState
+
+  // useReducer using simple variable numbr
+  // const [state, dispatch] = useReducer(reducer, 0);
+  // function reducer(state, action) {
+  //   if (action.type == "Increment") {
+  //     return state + action.payload;
+  //   } else if (action.type == "decrement") {
+  //     return state - action.payload;
+  //   }
+  // }
+
+  // useReducer using the complex object
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   function reducer(state, action) {
     if (action.type == "Increment") {
-      return state + action.payload;
+      return { ...state, count: state.count + action.payload };
     } else if (action.type == "decrement") {
-      return state - action.payload;
+      return { ...state, count: state.count - action.payload };
     }
   }
-
   return (
     <>
-      <div>
+      {/* <div>
         <p>{count}</p>
         <button onClick={() => setCount(count + 1)}>Increment</button>
         <button onClick={() => setCount(count - 1)}>decrement</button>
-      </div>
+      </div> */}
       <div>
-        <p>{state}</p>
+        {/* <p>{state}</p> */}
+        <p>{state.count}</p>
         <button onClick={() => dispatch({ type: "Increment", payload: 1 })}>
           Increment
         </button>
