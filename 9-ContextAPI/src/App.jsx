760bcd5909b7120ca9ff3d71e904ddf755313037
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-26 16:18:32 +0500

    refactor: replace UserContext with UserProvider for improved context management

--- 9-ContextAPI/src/App.jsx
+++ 9-ContextAPI/src/App.jsx
@@ -1,17 +1,16 @@
-import { createContext, useState } from "react";
 import Checkout from "./components/Checkout";
 import Login from "./components/login";
-
-export const UserContext = createContext();
+import Logout from "./components/Logout";
+import { UserProvider } from "./context/UserContext";
 
 export default function App() {
-  const [user, setUser] = useState("guest");
   return (
     <>
-    <UserContext.Provider value={{ user,setUser }}>
-      <Login />
-      <Checkout />
-    </UserContext.Provider>
+      <UserProvider>
+        <Login />
+        <Checkout />
+        <Logout />
+      </UserProvider>
     </>
   );
 }
